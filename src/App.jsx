4ffab3decb9d56e6241cf9e2080f61c8718b5b54
SantiagoScetti <santiscetti22@gmail.com>
2024-01-16 21:52:30 -0300

    0.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>La tienda de valeria</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Cuantas veces intentamos esto {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+         LA RE CAJETA DE LA LORA
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        PORFIN NUNCA MÁS
       </p>
     </>
   )
